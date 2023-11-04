f4ac5bb56f3fee9dcd60d8c86c3332e7f727436a
Augasty <sayaksengupta140@gmail.com>
2023-11-04 16:44:51 +0530

    done

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import { NAME } from "../constants/name"
+
+const Card = () => {
+  return (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY {NAME}!</h3>
+
+        <h5 className="suggest">click the card dost</h5>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <p>Bhaai,</p>
+        <p>
+          Happy birthday {NAME}!! Many many happy returns of the day. 
+          Please treat dis.
+        </p>
+        <p className="name">Augasty</p>
+        <p className="flexing">Yep, I just flexed my technical skills.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
